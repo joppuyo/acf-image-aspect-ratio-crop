96ef876a735be4eddbb6c8a0af1b5a6ec26444ad
Johannes Siipola <johannes@siipo.la>
2018-09-09 23:39:46 +0300

    Fix aspect ratio calculation logic

--- acf-image-aspect-ratio-crop.php
+++ acf-image-aspect-ratio-crop.php
@@ -68,16 +68,11 @@ if (!class_exists('npx_acf_plugin_image_aspect_ratio_crop')):
 
                 $image_data = wp_get_attachment_metadata($data['id']);
 
-                // If the difference between the images is less than half a percentage
-                if (
-                    ($image_data['height'] - $data['height']) <
-                    (
-                        $data['height'] * 0.005 &&
-                            ($image_data['height'] - $data['height']) <
-                                ($data['height'] * 0.005)
-                    )
+                // If the difference between the images is less than half a percentage, use the original image
+                // prettier-ignore
+                if ($image_data['height'] - $data['height'] < ($image_data['height'] * 0.005 &&
+                    $image_data['width'] - $data['width'] < $image_data['width'] * 0.005)
                 ) {
-                    // The cropped image is the same as original
                     wp_send_json(['id' => $data['id']]);
                     wp_die();
                 }
