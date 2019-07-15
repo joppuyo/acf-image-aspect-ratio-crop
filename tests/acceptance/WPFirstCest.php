aecb8933f48eaf342887e39d2d28550b506b22b9
Johannes Siipola <johannes@siipo.la>
2019-07-15 22:31:40 +0300

    Verify image

--- tests/acceptance/WPFirstCest.php
+++ tests/acceptance/WPFirstCest.php
@@ -12,6 +12,7 @@ class WPFirstCest
         $I->loginAsAdmin();
         $I->amOnPluginsPage();
         $I->activatePlugin('advanced-custom-fields-pro');
+        $I->saveSessionSnapshot('login');
     }
 
     /**
@@ -19,7 +20,7 @@ class WPFirstCest
      */
     public function activatePlugin(AcceptanceTester $I)
     {
-        $I->loginAsAdmin();
+        $I->loadSessionSnapshot('login');
         $I->amOnPluginsPage();
         $I->activatePlugin('acf-image-aspect-ratio-crop');
     }
@@ -29,7 +30,7 @@ class WPFirstCest
      */
     public function createNewField(AcceptanceTester $I)
     {
-        $I->loginAsAdmin();
+        $I->loadSessionSnapshot('login');
         $I->amOnAdminPage('edit.php?post_type=acf-field-group');
         $I->wait(1);
         $I->click('#acf-field-group-wrap > a');
@@ -46,7 +47,7 @@ class WPFirstCest
 
     public function createPost(AcceptanceTester $I)
     {
-        $I->loginAsAdmin();
+        $I->loadSessionSnapshot('login');
         $I->amOnAdminPage('post-new.php');
         $I->fillField("#post-title-0", "Test Post");
         $I->click('Add Image');
@@ -57,12 +58,39 @@ class WPFirstCest
         $I->waitForElementClickable('div.media-toolbar-primary.search-form > button', 10); // secs
         $I->click('div.media-toolbar-primary.search-form > button');
         $I->waitForElementVisible('.js-acf-image-aspect-ratio-crop-modal', 10);
-        $I->wait(5);
+        $I->waitForElementVisible('.cropper-crop-box', 10);
         $I->click('.js-acf-image-aspect-ratio-crop-crop');
         $I->waitForElementNotVisible('.js-acf-image-aspect-ratio-crop-modal', 10);
+        $this->verifyImage($I);
         $I->click('Publish…');
         $I->waitForElementVisible('.editor-post-publish-button', 10);
         $I->click('.editor-post-publish-button');
-        $I->wait(10);
+        $I->waitForText('Post published.');
+        $I->amOnAdminPage('edit.php');
+        $I->see('Test Post');
+    }
+
+    public function checkImage(AcceptanceTester $I)
+    {
+        $I->loadSessionSnapshot('login');
+        $I->amOnAdminPage('edit.php');
+        $I->click('Test Post');
+        $this->verifyImage($I);
+    }
+
+    private function verifyImage(AcceptanceTester $I)
+    {
+        $I->waitForElementVisible('.acf-field.acf-field-image-aspect-ratio-crop', 10);
+        $I->moveMouseOver('.acf-field.acf-field-image-aspect-ratio-crop div img');
+        $I->click('.acf-icon.-pencil.dark');
+        $I->waitForJqueryAjax();
+        $I->waitForElementVisible('label[data-setting="url"] input');
+        $filename = $I->grabValueFrom('label[data-setting="url"] input');
+        PHPUnit_Framework_Assert::assertContains('-aspect-ratio-16x9', $filename);
+        PHPUnit_Framework_Assert::assertEquals(
+            md5(file_get_contents(__DIR__ . '../../_data/cropped.jpg')),
+            md5(file_get_contents($filename))
+        );
+        $I->click('button.media-modal-close');
     }
 }
