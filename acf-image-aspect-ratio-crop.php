a0aa1421468db01636ba87ea6641bdc765b41197
Johannes Siipola <johannes.siipola@creuna.fi>
2020-07-28 19:09:26 +0300

    Add support for WPGraphQL plugin

--- acf-image-aspect-ratio-crop.php
+++ acf-image-aspect-ratio-crop.php
@@ -471,6 +471,35 @@ class npx_acf_plugin_image_aspect_ratio_crop
         }
 
         add_action('aiarc_delete_unused_attachments', [$this, 'delete_unused_attachments']);
+
+        add_filter('wpgraphql_acf_supported_fields', function ($supported_fields) {
+            array_push($supported_fields, 'image_aspect_ratio_crop');
+            return $supported_fields;
+        });
+
+        add_filter('wpgraphql_acf_register_graphql_field', function ($field_config, $type_name, $field_name, $config) {
+
+            // How to add new WPGraphQL fields is super undocumented, I used this code as a base
+            // https://github.com/wp-graphql/wp-graphql/issues/214#issuecomment-653141685
+
+            $acf_field = isset($config['acf_field']) ? $config['acf_field'] : null;
+            $acf_type = isset($acf_field['type']) ? $acf_field['type'] : null;
+
+            $resolve = $field_config['resolve'];
+
+            if ($acf_type == "image_aspect_ratio_crop") {
+                $field_config = [
+                    'type' => 'MediaItem',
+                    'resolve' => function ($root, $args, $context, $info) use ($resolve) {
+                        $value = $resolve($root, $args, $context, $info);
+                        return WPGraphQL\Data\DataSource::resolve_post_object((int)$value, $context);
+                    }
+                ];
+            }
+
+            return $field_config;
+        }, 10, 4);
+
     }
 
     /*
