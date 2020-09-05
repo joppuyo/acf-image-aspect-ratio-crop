db93b1735403ee5ddcf383642500636ae5c884b8
Johannes Siipola <johannes.siipola@creuna.fi>
2020-09-05 20:26:07 +0300

    Add tests for pixel crop

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -4,7 +4,54 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use AcceptanceTester;
+use PHPUnit_Framework_Assert;
+
 class Acceptance extends \Codeception\Module
 {
+    function verifyImage(
+        AcceptanceTester $I,
+        $comparison_image,
+        $width,
+        $height
+    ) {
+        $I->waitForElementVisible(
+            '.acf-image-uploader-aspect-ratio-crop div img',
+            10
+        );
+        $I->moveMouseOver(
+            '.acf-field.acf-field-image-aspect-ratio-crop div img'
+        );
+        $I->click('.acf-icon.-pencil.dark');
+        $I->waitForJqueryAjax();
+        // This changed in WP 5.3
+        try {
+            $I->waitForElementVisible('#attachment-details-copy-link');
+            $url = $I->grabValueFrom('#attachment-details-copy-link');
+        } catch (Exception $exception) {
+            $I->waitForElementVisible('label[data-setting="url"] input');
+            $url = $I->grabValueFrom('label[data-setting="url"] input');
+        }
+        // Image path is sometimes thumbnail???
 
+        $filename = $I->grabTextFrom('.filename');
+
+        $url = explode('/', $url);
+        array_pop($url);
+        array_push($url, $filename);
+        $url = implode('/', $url);
+
+        codecept_debug($filename);
+        PHPUnit_Framework_Assert::assertContains(
+            "-aspect-ratio-$width-$height",
+            $url
+        );
+        PHPUnit_Framework_Assert::assertEquals(
+            json_encode(
+                getimagesize(__DIR__ . "../../../_data/$comparison_image")
+            ),
+            json_encode(getimagesize($url))
+        );
+        $I->click('button.media-modal-close');
+    }
 }
