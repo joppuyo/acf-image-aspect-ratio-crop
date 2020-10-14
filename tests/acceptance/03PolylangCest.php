00ce61b331c4d2c23929b2f7388f7ee30509b97d
Johannes Siipola <johannes.siipola@creuna.fi>
2020-10-14 17:29:39 +0300

    Add Polylang test

---
+++ tests/acceptance/03PolylangCest.php
@@ -0,0 +1,88 @@
+<?php
+
+class PolylangCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function activateAcf(AcceptanceTester $I)
+    {
+        $I->cleanUploadsDir();
+        $I->cli(['core', 'update-db']);
+        $I->dontHavePostInDatabase([]);
+        $I->loginAsAdmin();
+        $I->amOnPluginsPage();
+        $I->cli([
+            'plugin',
+            'install',
+            str_rot13(
+                'uggcf://49gowgy57reib3jkj.o-pqa.arg/nps/cbylynat-ceb.2.8.2.mvc'
+            ),
+            '--force',
+        ]);
+        $I->activatePlugin('advanced-custom-fields-pro');
+        $I->activatePlugin('polylang-pro');
+        $I->saveSessionSnapshot('login');
+    }
+
+    /**
+     * @depends activateAcf
+     */
+    public function activatePlugin(AcceptanceTester $I)
+    {
+        $I->loadSessionSnapshot('login');
+        $I->amOnPluginsPage();
+        $I->activatePlugin('acf-image-aspect-ratio-crop');
+    }
+
+    /**
+     * @depends activatePlugin
+     */
+    public function setUpPolylang(AcceptanceTester $I)
+    {
+        $I->loadSessionSnapshot('login');
+        $I->amOnPluginsPage();
+        $I->click('Run the Setup Wizard');
+        $I->click('Continue');
+        $I->click('.ui-selectmenu-button');
+        $I->pressKey('.ui-selectmenu-button', 'e');
+        $I->click('#ui-id-27');
+        $I->click('Add new language');
+        $I->wait(1);
+        $I->click('.ui-selectmenu-button');
+        $I->wait(1);
+        $I->pressKey('.ui-selectmenu-button', 's');
+        $I->wait(1);
+        $I->click('#ui-id-172');
+        $I->click('Add new language');
+        $I->click('Continue');
+        $I->click('.pll-wizard-service-toggle');
+        $I->click('Continue');
+        $I->click('Return to the Dashboard');
+        $I->wait(1);
+    }
+
+    /**
+     * @depends setUpPolylang
+     */
+    public function createNewField(AcceptanceTester $I)
+    {
+        $I->loadSessionSnapshot('login');
+        $I->amOnAdminPage('edit.php?post_type=acf-field-group');
+        $I->wait(1);
+        $I->click('a.page-title-action');
+        $I->fillField('#title', 'Post');
+        $I->click(
+            '#acf-field-group-fields > div > div > ul.acf-hl.acf-tfoot > li > a'
+        );
+        $I->fillField('Field Label', 'Crop Image');
+        $I->selectOption('Field Type', 'Image Aspect Ratio Crop');
+        $I->waitForText('Width');
+        $I->fillField('Width', '16');
+        $I->fillField('Height', '9');
+        $I->scrollTo('#submitdiv');
+        $I->click('Publish');
+    }
+}
