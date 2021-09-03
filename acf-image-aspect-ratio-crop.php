cf98a7cccec5bc0017fdfb06d5b59f90e8a45485
Johannes Siipola <johannes@siipo.la>
2021-09-03 17:58:12 +0300

    Test coverage

--- acf-image-aspect-ratio-crop.php
+++ acf-image-aspect-ratio-crop.php
@@ -12,6 +12,11 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: acf-image-aspect-ratio-crop
 */
 
+// Load c3 in CI environment for code coverage
+if (file_exists(__DIR__ . '/c3.php')) {
+    require_once __DIR__ . '/c3.php';
+}
+
 // exit if accessed directly
 if (!defined('ABSPATH')) {
     exit();
