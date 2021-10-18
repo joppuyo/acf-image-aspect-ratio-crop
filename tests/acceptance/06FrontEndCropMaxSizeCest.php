acf7eabf7b29b07c52844cee2482187cec6ae9b1
Johannes Siipola <johannes@siipo.la>
2021-10-18 21:14:38 +0300

    Update 06FrontEndCropMaxSizeCest.php

--- tests/acceptance/06FrontEndCropMaxSizeCest.php
+++ tests/acceptance/06FrontEndCropMaxSizeCest.php
@@ -104,7 +104,7 @@ class FrontEndCropMaxSizeCest
         );
         //$I->wait(10);
         $I->click('Update');
-        $I->waitForElement('#message');
+        $I->waitForElement('#message', 60);
         //$I->wait(10);
     }
 
